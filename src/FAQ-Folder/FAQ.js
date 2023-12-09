5fb76301c44698d75b6e144b85a2c5e95c32c6e9
Kh-Noufal <147395851+Khokha-189433@users.noreply.github.com>
2023-12-09 07:18:27 -0500

    test

--- src/FAQ-Folder/FAQ.js
+++ src/FAQ-Folder/FAQ.js
@@ -14,16 +14,16 @@ export default function FAQ()
              </div>   
         
             <div className="all-ASKEDQUESTIONS" > 
-            
               <h4 id='paragraf'  style={{color: "#1f2278ce" , textAlign:"center"}}>FREQUENTLY ASKED QUESTIONS</h4>
-              <form     > 
-              <div   style={{ display:"flex" ,justifyContent:"center"}}>
-               <div className="oneASKEDQUESTIONS">
+          
+              <form> 
+              <div className='div-name' > 
+                 <div className="oneASKEDQUESTIONS">
                      <h4  style={{marginTop:"19px" ,color: "#1f2278ce" ,textAlign:"center" }}> Present Illnes<h1 class="heading"> </h1>
                      </h4> 
                        <hr style={{ margin:"25px"}}></hr>
                        <h4  style={{marginTop:"19px"   }}>Type (s) of the main lesion at presentation</h4> 
-                       <select    style={{width: "90%" ,padding:"12px" , border:"none" , background:"white"}} name ="select"  >
+                       <select    style={{width: "90%" ,padding:"11px" , border:"none" , background:"white"}} name ="select"  >
                            <option value="">  </option>
                            <option> uigsufaus;idsahuifaoh'iha'sofh'soihfs </option>
                            <option> uigsufaus;idsahuifaoh'iha'sofh'soihfs </option>
@@ -44,23 +44,23 @@ export default function FAQ()
           <input type="radio" name="group2" value="option3"/> Option 3
         </label>
     </fieldset>
-    <textarea  name='notes' rows={4} cols={62}  placeholder=' Write Notes' > </textarea>
+    <textarea  name='notes' rows={3} cols={72}  placeholder=' Write Notes'style={{resize: "none"}}> </textarea>
         <br />
         <h5> Deformity </h5>  
-        <input type='text' style={{width:"34.6rem" ,height:"2.9REM"}} />
+        <input type='text' style={{width:"39.9rem" ,height:"2.9REM"}} />
         <br />
         {/* */}
         <h5> Position of the lesion </h5>  
        
           <div  style={{display:"flex" ,justifyContent:"center" ,alignItems:"center", paddingRight:"3.4rem" }} >
-            <select    style={{width: "70%", padding:"12PX" , border:"none" , background:"white" }} name='select' value={value}  onChange={event=>setfaqinput(...value,event.target.value)} >
+            <select    style={{width: "40%", padding:"12PX" , border:"none" , background:"white" }} name='select' value={value}  onChange={event=>setfaqinput(...value,event.target.value)} >
                                       <option value="">  </option>
                                       <option> dfauihvifauhaiu </option>
                                       <option> dfisghudfshgodf </option>
                                       <option> fiufrehoijofirj </option>
                                       <option> fdjiudoinhodfii </option>
             </select> 
-            <select    style={{width: "50%" ,padding:"12px" , border:"none" , background:"white",marginLeft:"40px"  }} name='select' value={value}  onChange={event=>setfaqinput(...value, event.target.value)}  >
+            <select    style={{width: "40%" ,padding:"12px" , border:"none" , background:"white",marginLeft:"30px"  }} name='select' value={value}  onChange={event=>setfaqinput(...value, event.target.value)}  >
                                       <option value="">  </option>
                                       <option > 1 </option>
                                       <option> 2 </option>
@@ -100,12 +100,12 @@ export default function FAQ()
             </div>
         </div>
         <h5 style={{ margin:"4px 0px -10px 0px "}}> Write Notes</h5>
-        <textarea  name='notes'   rows={2,2} cols={61} placeholder=' Write Notes' > </textarea>
+        <textarea  name='notes'   rows={2} cols={61} placeholder=' Write Notes' style={{resize: "none"}}  > </textarea>
         <br />
          
                  
         </div>
-   
+   <div />
       {/* pppppppppppppppppppppppppppppppppppppppppppppppppppppp */}
 
       < div className="oneASKEDQUESTIONS">
@@ -136,16 +136,60 @@ export default function FAQ()
                                   <option> hhhhhhhhhhhh </option>
                                   <option> jjjjjjjjjjjj </option>
         </select> 
-
         </div>
 
-   
+        <div className='box-one' style={{height:"auto"}} >
+        <h5> Other consultation(s) </h5>  
+        <textarea  style={{ resize: "none" , height:"25rem"}}  cols={55}> ADD YOUR NOTES </textarea>
+        </div>
+        
         </div>
       </div>
-      </form>     
+      {/* ============================= */}
+     <div className='div-name' > 
+      < div className="oneASKEDQUESTIONS">
+         <h4  style={{marginTop:"19px" ,color: "#1f2278ce" ,textAlign:"center" }}> The plan of management</h4> 
+         <hr style={{ margin:"25px"}}></hr>
+         <h5>Description of the planned procedure </h5> 
+        <input type='text' style={{width:"33.6rem" ,height:"2.9REM"}} />
+        <h5>The Joint should be fixed by   </h5> 
+        <input type='text' style={{width:"33.6rem" ,height:"2.9REM"}} />
+        <h5>The amputation should be </h5>  
+        <input type='text' style={{width:"33.6rem" ,height:"2.9REM"}} />
+        
+        <h5>Prescription IV </h5>  
+        <select    style={{width: "90%" ,padding:"12px" , border:"none" , background:"white" ,width:"33.7rem"}} name='select'  >
 
-     </div>
+                                  <option value="">  </option>
+                                  <option>  joijpijp;oj</option>
+                                  <option> kkkkkkkkkkkk </option>
+                                  <option> hhhhhhhhhhhh </option>
+                                  <option> jjjjjjjjjjjj </option>
+        </select> 
+         <h5> The date of the last vascular consultation </h5> 
+          <input type='date' style={{width:"33.6rem" ,height:"2.9REM"}} />
+         
+          <h5>P The following laboratory tests are required </h5>  
+        <select    style={{width: "90%" ,padding:"12px" , border:"none" , background:"white" ,width:"33.7rem"}} name='select'  >
 
+                                  <option value="">  </option>
+                                  <option>  joijpijp;oj</option>
+                                  <option> kkkkkkkkkkkk </option>
+                                  <option> hhhhhhhhhhhh </option>
+                                  <option> jjjjjjjjjjjj </option>
+        </select> 
+        
+        </div>
+       {/* ============================= */}
+        < div className="oneASKEDQUESTIONS">
+         <h4  style={{marginTop:"19px" ,color: "#1f2278ce" ,textAlign:"center" }}> The plan of management</h4> 
+             
+  
+          </div>
+        </div>
+     </form>     
+   
+  </div>
 </div>
         
         
